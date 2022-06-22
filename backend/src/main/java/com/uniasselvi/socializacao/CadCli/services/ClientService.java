d3dabc9aa2c12157173f4889e1b5825868a7b5a4
Elissandro <elissandroa@hotmail.com>
2022-06-22 20:36:40 -0300

    First Homolog

--- backend/src/main/java/com/uniasselvi/socializacao/CadCli/services/ClientService.java
+++ backend/src/main/java/com/uniasselvi/socializacao/CadCli/services/ClientService.java
@@ -48,7 +48,7 @@ public class ClientService {
 	@Transactional
 	public ClientDTO update(Long id, ClientDTO dto) {
 		try {
-			Client entity = repository.getReferenceById(id);
+			Client entity = repository.getById(id);
 			copyDtoToEntity(dto, entity);
 			entity = repository.save(entity);
 			return new ClientDTO(entity);
