d52d3fcf68bcaa79acf22b6a1f825dfaaa83df7b
Elissandro <elissandroa@hotmail.com>
2022-06-21 21:01:55 -0300

    Start Project

---
+++ backend/src/main/java/com/uniasselvi/socializacao/CadCli/CadCliApplication.java
@@ -0,0 +1,13 @@
+package com.uniasselvi.socializacao.CadCli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadCliApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/uniasselvi/socializacao/CadCli/CadCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniasselvi.socializacao.CadCli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
